a61062709125df0bf697e7627462653508983e64
boc <893296042@qq.com>
2019-08-28 09:35:56 +0800

    2019-08-28 in work computer auto push

--- my compuer github push.bat
+++ my compuer github push.bat
@@ -1,9 +1,9 @@
-cd C:\Users\我什么都不知道\Desktop\杂物\note
+cd C:\Users\admin\Desktop\tmp\note
 git add .
-git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in my computer auto push"
+git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in work computer auto push"
 git push
-cd C:\Users\我什么都不知道\Desktop\杂物\log
+cd C:\Users\admin\Desktop\tmp\log
 git add .
-git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in my computer auto push"
+git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in work computer auto push"
 git push
 pause
