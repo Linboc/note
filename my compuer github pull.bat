d8a632092547eeb1086aa2a447992a870f310282
boc <893296042@qq.com>
2020-05-28 14:32:25 +0800

    2020-05-28 in my computer auto push

--- my compuer github pull.bat
+++ my compuer github pull.bat
@@ -1,5 +1,3 @@
 cd C:\Users\我什么都不知道\Desktop\杂物\note
 git pull
-cd C:\Users\我什么都不知道\Desktop\杂物\log
-git pull
 pause 
--- my compuer github push.bat
+++ my compuer github push.bat
@@ -2,8 +2,4 @@ cd C:\Users\我什么都不知道\Desktop\杂物\note
 git add .
 git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in my computer auto push"
 git push
-cd C:\Users\我什么都不知道\Desktop\杂物\log
-git add .
-git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2% in my computer auto push"
-git push
 pause 
